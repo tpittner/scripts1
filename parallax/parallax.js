e678901cb22f16d0ace3bd5df961a99abd4d325c
tpittner <timo.pittner@swarco.de>
2015-03-24 15:33:10 +0100

    Update tpittner 20150324

--- parallax/parallax.js
+++ parallax/parallax.js
@@ -21,7 +21,23 @@ var contentComposite = tabris.create("Composite", {
 
 tabris.create("TextView", {
   layoutData: {left: MARGIN, right: MARGIN, top: MARGIN},
-  text: "Etiam nisl nisi, egestas quis lacus ut, tristique suscipit metus. In vehicula lectus " +
+//  text: "Etiam nisl nisi, egestas quis lacus ut, tristique suscipit metus. In vehicula lectus " +
+//        "metus, at accumsan elit fringilla blandit. Integer et quam sed dolor pharetra " +
+//        "molestie id eget dui. Donec ac libero eu lectus dapibus placerat eu a tellus. Fusce " +
+//        "vulputate ac sem sit amet bibendum. Pellentesque euismod varius purus pharetra. Sed " +
+//        "vitae ipsum sit amet risus vehicula euismod in at nunc. Sed in viverra arcu, id " +
+//        "blandit. Praesent sagittis quis nisl id molestie. Donec dignissim, nisl id volutpat " +
+//        "consectetur, massa diam aliquam lectus, sed euismod leo elit eu justo. Integer vel " +
+//        "ante sapien.\n\nNunc sit amet blandit tellus, sed neque. Proin vel elementum augue. " +
+//        "Quisque gravida nulla nisl, at fermentum turpis euismod in. Maecenas tortor at ante " +
+//        "vulputate iaculis at vitae sem. Nulla dui erat, viverra eget mauris in, sodales " +
+//        "mollis. Integer rhoncus suscipit mi in pulvinar. Nam metus augue, dictum a egestas " +
+//        "ut, gravida eget ipsum. Nunc nisl, mollis et mauris in, venenatis blandit magna. " +
+//        "Nullam scelerisque tellus lacus, in lobortis purus consectetur sed. Etiam pulvinar " +
+//        "sapien vel nibh vehicula, in lacinia odio pharetra. Duis tincidunt metus a semper " +
+//        "auctor. Sed nec consequat augue, id vulputate orci. Nunc metus nulla, luctus id " +
+//        "porttitor nec, sed lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+text: "Stefan Sans ist ein Feinschmecker!!!, tristique suscipit metus. In vehicula lectus " +
         "metus, at accumsan elit fringilla blandit. Integer et quam sed dolor pharetra " +
         "molestie id eget dui. Donec ac libero eu lectus dapibus placerat eu a tellus. Fusce " +
         "vulputate ac sem sit amet bibendum. Pellentesque euismod varius purus pharetra. Sed " +
